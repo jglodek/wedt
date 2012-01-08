9cebb669eea281be96dca9f79bbca27f518646c0
Jacek Głodek <jacek.glodek@gmail.com>
2012-01-08 15:43:05 +0100

    application herlp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	
+	def category_name(category)
+		if(category and category.category)
+			"#{category_name(category.category)} : #{category.name}"
+		else
+			"#{category.name}" if category
+		end
+	end
 end

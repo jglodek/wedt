ef71d9daea23718afa94f45f449b092192bde661
Jacek Głodek <jacek.glodek@gmail.com>
2012-01-18 20:54:25 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Wedt::Application.routes.draw do
 	resources :bills
 	resources :products
 	
-	get '/product/suggest_category/:id' => 'products#suggest_category'
+	get '/product/suggest_category/' => 'products#suggest_category'
 	
 	root :to => 'bills#index'
 end
